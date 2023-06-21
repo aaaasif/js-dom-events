83fd5c8f936ba7f73f61ee7be58e4e203e83ee62
Abdullah Al Asif <alexasif4@gmail.com>
2023-06-21 13:25:31 +0600

    js dom commit

---
+++ js/events.js
@@ -0,0 +1,21 @@
+console.log("This is separate js file");
+
+// Option 2
+function makeRed () {
+    document.body.style.backgroundColor = 'Red';
+}
+
+// option 3
+const makeBlueButton = document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+        function makeBlue() {
+            document.body.style.backgroundColor = "blue";
+        }
+
+// option 4
+const purpleButton = document.getElementById('make-purple');
+        purpleButton.onclick = function makePurple() {
+            document.body.style.backgroundColor = 'purple';
+        }
+
+// option 5
